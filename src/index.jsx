2d70832e1f33f7a225e621db6af398ebcda18d67
tyler suderman <tcsuder@gmail.com>
2017-07-20 11:44:31 -0700

    set up remote server and HRM

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <div>
-    <h1>React Legend</h1>
-  </div>,
-  document.getElementById('react-app-root')
-);
+import App from './components/App';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
